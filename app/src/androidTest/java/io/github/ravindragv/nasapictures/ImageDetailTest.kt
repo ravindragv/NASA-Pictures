7565ea2c25cfaf9aaf1e3974fe2dda00eea78225
Ravindra GV <ravindra.gv@gmail.com>
2021-10-09 15:43:39 +0530

    Add UI test for ImageDetailTest fragment
    + Add the following UI test cases -
      1. Ensure that all views are displayed completely
      2. Check whether the all the content is show wrt to the first image in the list
      3. Check whether the all the content is show wrt to the last image in the list
      4. Test scenario where the copyright text view is set to GONE, along with other UI elements
    + Update build.gradle to include fragment testing module
    + Make androidx.test:core part of debugImplementation to support
      fragment testing package

---
+++ app/src/androidTest/java/io/github/ravindragv/nasapictures/ImageDetailTest.kt
@@ -0,0 +1,111 @@
+package io.github.ravindragv.nasapictures
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.github.ravindragv.nasapictures.activities.MainActivity
+import io.github.ravindragv.nasapictures.fragments.ImageDetailFragment
+import io.github.ravindragv.nasapictures.factory.ImageFragmentFactory
+import io.github.ravindragv.nasapictures.model.ImageMetaData
+import io.github.ravindragv.nasapictures.model.ImageMetaDataViewModel
+import org.hamcrest.core.IsNot.not
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.text.SimpleDateFormat
+
+@RunWith(AndroidJUnit4::class)
+class ImageDetailTest {
+    @get:Rule
+    val activityScenarioRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(
+        MainActivity::class.java
+    )
+    private lateinit var imageMetaDataViewModel: ImageMetaDataViewModel
+    private lateinit var imageFragmentFactory: ImageFragmentFactory
+    private lateinit var imageMetaDataList: List<ImageMetaData>
+
+    @Before
+    fun setUp() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            imageMetaDataViewModel = activity
+                .defaultViewModelProviderFactory
+                .create(ImageMetaDataViewModel::class.java)
+            imageMetaDataList = imageMetaDataViewModel.getList()
+            imageFragmentFactory = ImageFragmentFactory(imageMetaDataViewModel)
+        }
+    }
+
+    @Test
+    fun testAllViewsAreVisible() {
+        launchFragmentInContainer<ImageDetailFragment>(factory = imageFragmentFactory)
+
+        onView(withId(R.id.iv_full_image)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.tv_title)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.tv_copyright)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.tv_date)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.tv_explanation)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun testContentForFirstPosition() {
+        imageMetaDataViewModel.currentPosition = 0
+
+        launchFragmentInContainer<ImageDetailFragment>(factory = imageFragmentFactory)
+        onView(withId(R.id.tv_title))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].title)))
+        onView(withId(R.id.tv_copyright))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].copyright)))
+        val date = SimpleDateFormat("yyyy-MM-dd")
+            .format(imageMetaDataList[imageMetaDataViewModel.currentPosition].date)
+        onView(withId(R.id.tv_date))
+            .check(matches(withText(date)))
+        onView(withId(R.id.tv_explanation))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].explanation)))
+
+    }
+
+    @Test
+    fun testContentForLastPosition() {
+        imageMetaDataViewModel.currentPosition = imageMetaDataList.size - 1
+
+        launchFragmentInContainer<ImageDetailFragment>(factory = imageFragmentFactory)
+        onView(withId(R.id.tv_title))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].title)))
+        onView(withId(R.id.tv_copyright))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].copyright)))
+        val date = SimpleDateFormat("yyyy-MM-dd")
+            .format(imageMetaDataList[imageMetaDataViewModel.currentPosition].date)
+        onView(withId(R.id.tv_date))
+            .check(matches(withText(date)))
+        onView(withId(R.id.tv_explanation))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].explanation)))
+    }
+
+    @Test
+    fun testContentWithoutCopyright() {
+        imageMetaDataViewModel.currentPosition = 2
+
+        launchFragmentInContainer<ImageDetailFragment>(factory = imageFragmentFactory)
+        onView(withId(R.id.tv_title))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].title)))
+        onView(withId(R.id.tv_copyright)).check(matches(not(isCompletelyDisplayed())))
+        val date = SimpleDateFormat("yyyy-MM-dd")
+            .format(imageMetaDataList[imageMetaDataViewModel.currentPosition].date)
+        onView(withId(R.id.tv_date))
+            .check(matches(withText(date)))
+        onView(withId(R.id.tv_explanation))
+            .check(matches(withText(
+                imageMetaDataList[imageMetaDataViewModel.currentPosition].explanation)))
+    }
+}
